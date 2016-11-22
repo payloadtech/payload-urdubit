fed8f0e44594b6ba0f86b6374b13d001c0a12c40
amingilani <amin@gilani.me>
2016-11-22 17:44:36 +0500

    Create orders when you're told

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Setup express
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 var port = process.env.PORT || 3000;
 
 // Setup BlinkTrade
@@ -13,8 +14,51 @@ var blinktrade = new BlinkTradeWS({
 var heartbeats = require('heartbeats');
 var heart = heartbeats.createHeart(1000);
 
+// Setup Secret
+var secret = process.env.SECRET;
+
 console.log('Starting script');
 
+
+// setup the express app
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    // if the secret is correct skip to the next route;
+    if (req.query.secret === secret) {
+        next();
+    } else {
+        res.json({
+            message: 'Wtf? Are you high?',
+            status: '420'
+        });
+    }
+});
+
+app.post('/order', function(req, res) {
+    var amount = req.body.amount;
+    console.log(amount);
+    // send an order
+    blinktrade.sendOrder({
+        "side": "2", // Sell
+        "price": parseInt(1 * 1e8), // 1 PKR
+        "amount": parseInt(amount),
+        "symbol": "BTCPKR",
+    }).then(function(order) {
+        // respond with the order information
+        res.json(order);
+    }).catch(function(err) {
+        console.log(err);
+    });
+});
+
+
+app.get('/', function(req, res) {
+    res.json({
+        message: 'nothing here'
+    });
+});
+// setup the blinktrade behavior
 blinktrade
     .connect()
     // run a heartbeat
@@ -39,46 +83,35 @@ blinktrade
         // console.log(logged);
         console.log('logged in');
 
-
-        // blinktrade.sendOrder({
-        //     "side": "2", // Sell
-        //     "price": parseInt(1), // 1 PKR
-        //     "amount": parseInt((0.00505400 * 1e8).toFixed(0)),
-        //     "symbol": "BTCPKR",
-        // }).then(function(order) {
-        //   console.log(order);
-        //     // Sent
-        // }).catch(function(err) {
-        //   console.log(err);
-        // });
-
-        blinktrade.requestWithdraw({
-            "amount": parseInt(1200 * 1e8),
-            "currency": "PKR",
-            "method": "BankIslami",
-            "data": {
-                "AccountName": "A Girl Is No One",
-                "AccountNumber": "123477",
-                "Fees": "2"
-            }
-        });
-
+        // listing to execution reports
+        blinktrade.executionReport()
+            .on("EXECUTION_REPORT:NEW", function(data) {
+                console.log(data);
+            }).on("EXECUTION_REPORT:PARTIAL", function(data) {
+                console.log(data);
+            }).on("EXECUTION_REPORT:EXECUTION", function(data) {
+                console.log(data);
+            }).on("EXECUTION_REPORT:CANCELED", function(data) {
+                console.log(data);
+            }).on("EXECUTION_REPORT:REJECTED", function(data) {
+                console.log(data);
+            });
 
         app.listen(port, function() {
-            console.log('HTTP server running');
+            console.log('HTTP server running on PORT ' + port);
         });
+        // blinktrade.requestWithdraw({
+        //     "amount": parseInt(1200 * 1e8),
+        //     "currency": "PKR",
+        //     "method": "BankIslami",
+        //     "data": {
+        //         "AccountName": "A Girl Is No One",
+        //         "AccountNumber": "123477",
+        //         "Fees": "2"
+        //     }
+        // });
+        //
+        //
+    }).catch(function(err) {
+        console.error(err);
     });
-
-
-
-// // just having some fun
-// app.get('/', function(req, res) {
-//     res.json({
-//         message: 'Wtf? Are you high?',
-//         status: '420'
-//     });
-// });
-
-// app.post('/trades', function(req, res) {
-//
-// })
